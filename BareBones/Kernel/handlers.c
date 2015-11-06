ee043fba5b643e3d6d5e668dab6830f86fefa5dc
jmb <juanmbellini@hotmail.com>
2015-11-06 04:09:28 -0300

    SysCallsListo

--- BareBones/Kernel/handlers.c
+++ BareBones/Kernel/handlers.c
@@ -12,13 +12,12 @@
 static void (*interrupts[2])(void);
 static void (*syscalls[2]) (uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
 
-interrupts[0] = int08
-interrupts[1] = int09
-syscalls[0] = read;
-syscalls[1] = write;
 
 
 void irqDispatcher(dword irq, dword syscall) {
+
+	interrupts[0] = int08
+	interrupts[1] = int09
 
 	interrupts[irq]();
 	return;
@@ -36,6 +35,9 @@ void int09(void){
 
 
 void handler(uint64_t syscallId, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
+
+	syscalls[0] = read;
+	syscalls[1] = write;
 
 	syscalls[syscallId](arg1, arg2, arg3, arg4, arg5, arg6);
 	return;
