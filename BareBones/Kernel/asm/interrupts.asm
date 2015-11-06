ebec44032da79b4ff518d0afc14321893e37fd6d
jmb <juanmbellini@hotmail.com>
2015-11-06 02:46:27 -0300

    HolaBranch

--- BareBones/Kernel/asm/interrupts.asm
+++ BareBones/Kernel/asm/interrupts.asm
@@ -4,8 +4,8 @@ GLOBAL picMasterMask
 GLOBAL picSlaveMask
 
 
-GLOBAL turnOnSound
-GLOBAL turnOffSound
+; GLOBAL turnOnSound
+; GLOBAL turnOffSound
 
 
 
@@ -138,28 +138,30 @@ syscallHandler:
     call handler
     iret
 
-turnOnSound:
-
-mov al, 182         ;Prepares the speaker
-out 43h, al 
-
-mov rax, rdi        ; Copies the parameter value to rax (the frequency)
 
 
-out 42h, al         ; Output low byte.
-mov al, ah
-out 42h, al         ; Output high byte.
+; turnOnSound:
 
-in al, 61h
-or al, 3            ; Set bits 1 and 0. (Both last bits must be set to 1)
-out 61h, al         ; Send new value.
-ret                 ; Returns
+; mov al, 182         ;Prepares the speaker
+; out 43h, al 
+
+; mov rax, rdi        ; Copies the parameter value to rax (the frequency)
 
 
+; out 42h, al         ; Output low byte.
+; mov al, ah
+; out 42h, al         ; Output high byte.
 
-turnOffSound:
+; in al, 61h
+; or al, 3            ; Set bits 1 and 0. (Both last bits must be set to 1)
+; out 61h, al         ; Send new value.
+; ret                 ; Returns
 
-in al, 61h
-and al, -4          ; Reset bits 1 and 0. (Both last bits must be set to 0)
-out 61h, al         ; Send new value.
-ret                 ; Returns
+
+
+; turnOffSound:
+
+; in al, 61h
+; and al, -4          ; Reset bits 1 and 0. (Both last bits must be set to 0)
+; out 61h, al         ; Send new value.
+; ret                 ; Returns
--- BareBones/Kernel/include/handlers.h
+++ BareBones/Kernel/include/handlers.h
@@ -1,12 +1,16 @@
 #ifndef HANDLERS_H
 #define HANDLERS_H
-#include <types.h>
+//#include <types.h>
+#define READ 0
+#define WRITE 1
+#define SOUND 2
 
 void picMasterMask(); 
 void picSlaveMask();
 void irqDispatcher(dword irq, dword syscall);
 void int08();
 void int09();
+void handler(uint64_t syscallId, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6);
 
 
 #endif
