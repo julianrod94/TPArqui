8c33cc79cc1f13a7f7066ea3c71bd4e5e74574ca
jmb <juanmbellini@hotmail.com>
2015-11-06 04:26:48 -0300

    SysCallsListo

--- BareBones/Kernel/syscalls.c
+++ BareBones/Kernel/syscalls.c
@@ -60,7 +60,8 @@ void read(uint64_t fileDescriptor, uint64_t buffer, uint64_t size, uint64_t arg4
 		case TIMER:
 			readfromTimer((long *) buffer);
 			break;
-		default:
+		default
+			;
 	}
 	return;
 }
@@ -77,6 +78,7 @@ void write(uint64_t fileDescriptor, uint64_t buffer, uint64_t size, uint64_t aux
 			printInSpk((unsigned short) aux1);
 			break;
 		default:
+			;
 	}
 	return;
 
