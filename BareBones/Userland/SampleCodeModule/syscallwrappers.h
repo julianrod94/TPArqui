e0d6e09c7a65fc3c38d798d2d2a726c1398ae88e
jmb <juanmbellini@hotmail.com>
2015-11-07 02:33:22 -0300

    printf

--- BareBones/Userland/SampleCodeModule/syscallwrappers.h
+++ BareBones/Userland/SampleCodeModule/syscallwrappers.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-void read(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-void write(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+void read(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+void write(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
