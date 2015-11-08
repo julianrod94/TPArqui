62393f7393c15eb967462fc8f339fc9398922558
jmb <juanmbellini@hotmail.com>
2015-11-08 02:02:34 -0300

    newKbd

---
+++ BareBones/Kernel/newKbd.c
@@ -0,0 +1,55 @@
+#include <types.h>
+#include <interrupts.h>
+#include <handlers.h>
+
+#define SIZE 256
+
+/*typedef struct {
+	char buffer[SIZE];
+	int size;
+} tKbdQueue;
+
+static int kbdInit = 0;
+static tKbdQueue kbdQueue;
+
+tKbdQueue newKbdQueue() {
+
+}*/
+
+static uint64_t buffer[SIZE];
+static int bufferSize = 0;
+static int enqueueIdx = 0;
+static int dequeueIdx = 0;
+
+void keyboardISR(void) {
+
+	uint64_t num;
+
+	if (bufferSize == SIZE) {
+		return; // If the buffer is full, nothing should be saved
+	}
+	buffer[enqueueIdx++] = portRead();
+	bufferSize++;
+	if (enqueueIdx == SIZE) {
+		enqueueIdx = 0;
+	}
+}
+
+
+uint64_t dequeueKey(void) {
+
+	if (bufferSize == 0) {
+		return -1; // maybe we could just halt the machine
+	}
+	uint64_t result = buffer[dequeueIdx++];
+	bufferSize--;
+	if (dequeueIdx == SIZE) {
+		dequeueIdx = 0;
+	}
+	return result;
+}
+
+
+
+
+
